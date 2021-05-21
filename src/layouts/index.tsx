5d1ac7847d10d5b6e155d487166e20bea042e0d9
Nestor Cordova <cordova.jauregui@gmail.com>
2021-05-21 12:06:48 -0500

    add pokedex logo

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from "react";
-import { SafeAreaView, ImageBackground, StyleSheet } from "react-native";
+import { SafeAreaView, ImageBackground, StyleSheet, Image } from "react-native";
 const App: FC = ({ children }) => {
   return (
     <SafeAreaView style={styles.view}>
@@ -7,6 +7,10 @@ const App: FC = ({ children }) => {
         source={require("../../assets/pokedex-main.png")}
         style={styles.imageBackground}
       >
+        <Image
+          style={styles.Image}
+          source={require("../../assets/pokedex.png")}
+        />
         {children}
       </ImageBackground>
     </SafeAreaView>
@@ -20,6 +24,15 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
     paddingTop: 165,
     paddingBottom: 20,
+  },
+  Image: {
+    position: "absolute",
+    resizeMode: "contain",
+    top: 75,
+    right: 10,
+    width: 155,
+    height: 70,
+    fontSize: 30,
   },
   view: {
     flex: 1,
